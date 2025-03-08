c01c3422a5e8ea54697ca59f8a82918428102dba
Lucas Chiavicatti <lucaschiavicatti@hotmail.com>
2025-03-08 20:55:35 -0300

    chore: Projeto gerado com Spring Initializr

---
+++ src/main/java/com/mobiauto/backend/MobiautoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mobiauto.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiautoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiautoBackendApplication.class, args);
+	}
+
+}
